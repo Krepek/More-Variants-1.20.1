547849573f4a144269519e595c7dba76c1d8f159
Krepek <mastalerz.jan@hotmail.com>
2024-01-31 18:40:52 +0100

    Added Cherry Stick

---
+++ src/main/java/net/krepek/morevariants/item/ModItemGroups.java
@@ -0,0 +1,23 @@
+package net.krepek.morevariants.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.krepek.morevariants.MoreVariants;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup MORE_VARIANTS = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(MoreVariants.MOD_ID,"cherry"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.more_variants"))
+                    .icon( () -> new ItemStack(ModItems.CHERRY_STICK)).entries((displayContext, entries) -> {
+                            entries.add(ModItems.CHERRY_STICK);
+
+                    }).build());
+    public static void registerItemGroups() {
+        MoreVariants.LOGGER.info("Registering Item Griups  for" +MoreVariants.MOD_ID);
+    }
+}
--- src/main/java/net/krepek/morevariants/item/ModItems.java
+++ src/main/java/net/krepek/morevariants/item/ModItems.java
@@ -1,4 +1,26 @@
 package net.krepek.morevariants.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.krepek.morevariants.MoreVariants;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+    public static final Item CHERRY_STICK = registerItem("cherry_stick", new Item(new FabricItemSettings()));
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries)    {
+        entries.add(CHERRY_STICK);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MoreVariants.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems()   {
+        MoreVariants.LOGGER.info("Registering Mod Items for" + MoreVariants.MOD_ID);
+    }
 }
